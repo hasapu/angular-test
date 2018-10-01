055792c24167a9855235e9d4336191cf6f9a558d
hasapu <hari.sakti.putra@gmail.com>
2018-10-01 14:28:17 +0700

    example component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MaterialModule} from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ExampleComponent } from './example/example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule.forRoot([
+      {path: 'example', component: ExampleComponent}
+    ]),
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
