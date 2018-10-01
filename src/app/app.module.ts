b0e4641a7346ca8da1ed6241e053ca588c1f8702
Aulia Rahman <aul.rahman91@gmail.com>
2018-10-01 14:48:20 +0700

    Update css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {MaterialModule} from './material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { RouterModule, Routes } from "@angular/router";
+import { BrowserModule }        from '@angular/platform-browser';
+import { NgModule }             from '@angular/core';
+import { MaterialModule }       from './material.module';
+import { FlexLayoutModule }     from '@angular/flex-layout';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { AppComponent }            from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ExampleComponent } from './example/example.component';
+import { ExampleComponent }        from './example/example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExampleComponent
   ],
-  imports: [
+  imports     : [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     RouterModule.forRoot([
-      {path: 'example', component: ExampleComponent}
+      { path: 'example', component: ExampleComponent }
     ]),
     FlexLayoutModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers   : [],
+  bootstrap   : [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
